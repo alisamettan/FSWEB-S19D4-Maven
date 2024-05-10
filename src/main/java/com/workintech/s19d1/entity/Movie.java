23e4c9345dff0e576888166462928c99efbab726
alisamettan <alisamet_tan@hotmail.com>
2024-05-10 14:16:23 +0300

    .

---
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -0,0 +1,38 @@
+package com.workintech.s19d1.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "movie",schema = "fsweb")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "director_name")
+    private String directorName;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @Column(name = "release_date")
+    private Date releaseDate;
+
+    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinTable(name = "movie_actor",schema = "fsweb",joinColumns = @JoinColumn(name = "movie_id"),inverseJoinColumns = @JoinColumn(name = "actor_id"))
+    private List<Actor> actors;
+}
