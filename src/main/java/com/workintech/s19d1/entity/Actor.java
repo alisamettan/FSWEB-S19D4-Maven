0866c850b73f706646c259ed2697a0ae4db4690f
alisamettan <alisamet_tan@hotmail.com>
2024-05-11 15:51:23 +0300

    Solution.

--- src/main/java/com/workintech/s19d1/entity/Actor.java
+++ src/main/java/com/workintech/s19d1/entity/Actor.java
@@ -1,12 +1,15 @@
 package com.workintech.s19d1.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @AllArgsConstructor
@@ -18,12 +21,14 @@ public class Actor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "first_name")
+    @NotNull
     private String firstName;
 
     @Column(name = "last_name")
+    @NotNull
     private String lastName;
 
     @Column(name = "gender")
@@ -31,12 +36,21 @@ public class Actor {
     private Gender gender;
 
     @Column(name = "birth_date")
-    private Date birthDate;
+    private LocalDate birthDate;
 
-
+    @JsonManagedReference
     @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
     @JoinTable(name = "movie_actor",schema = "fsweb",joinColumns = @JoinColumn(name = "actor_id"),inverseJoinColumns = @JoinColumn(name = "movie_id"))
-    private List<Movie> movies=new ArrayList<>();
+    private List<Movie> movies;
+
+
+    public void addMovie(Movie movie){
+        if(movies==null){
+            movies=new ArrayList<>();
+
+        }
+        movies.add(movie);
+    }
 }
 
 
--- src/main/java/com/workintech/s19d1/entity/Movie.java
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -1,10 +1,14 @@
 package com.workintech.s19d1.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -21,18 +25,27 @@ public class Movie {
     private long id;
 
     @Column(name = "name")
+    @NotNull(message = "Film adı null olamaz")
     private String name;
 
     @Column(name = "director_name")
+    @NotNull(message = "Yönetmen adı null olamaz")
     private String directorName;
 
     @Column(name = "rating")
     private int rating;
 
     @Column(name = "release_date")
-    private Date releaseDate;
+    private LocalDate releaseDate;
 
+    @JsonBackReference
     @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
     @JoinTable(name = "movie_actor",schema = "fsweb",joinColumns = @JoinColumn(name = "movie_id"),inverseJoinColumns = @JoinColumn(name = "actor_id"))
     private List<Actor> actors;
+
+    public void addActor(Actor actor){
+        if(actors==null)
+            actors=new ArrayList<>();
+        actors.add(actor);
+    }
 }
